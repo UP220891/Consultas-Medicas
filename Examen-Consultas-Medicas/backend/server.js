48c1d8efb8b04f1b37103237bd94031928b60e62
Jeinny Melissa Villalobos Durón <up220891@alumnos.upa.edu.mx>
2025-07-14 19:39:07 -0600

    backend

---
+++ Examen-Consultas-Medicas/backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const connectDB = require('./config/database');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+connectDB(); // Conectar a la base de datos
+
+// Importar rutas
+/*const authRoutes = require('./routes/auth');
+const medicoRoutes = require('./routes/medicoRoutes');
+const pacienteRoutes = require('./routes/pacienteRoutes');
+const citaRoutes = require('./routes/citaRoutes');
+const especialidadRoutes = require('./routes/especialidadRoutes');*/
+
+/*app.use('/api/auth', authRoutes);
+app.use('/api/medicos', medicoRoutes);
+app.use('/api/pacientes', pacienteRoutes);
+app.use('/api/citas', citaRoutes);
+app.use('/api/especialidades', especialidadRoutes);*/
+
+app.get('/', (req, res) => {
+  res.send('¡Servidor de Consultas Médicas funcionando correctamente!');
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+});
